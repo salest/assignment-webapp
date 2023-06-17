eed5841af49c51454868b8faac69da72abf4ed0b
salest <tevin.sales@gmail.com>
2023-06-17 17:07:19 +0900

    updated. WorkDate is now saved as Timestamp and we can query for data by date range

--- FirebaseFunctionsExample/functions/src/index.ts
+++ FirebaseFunctionsExample/functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+//Need to import Timestamp from google-cloud and not admin
+import { Timestamp } from "@google-cloud/firestore";
 
 admin.initializeApp({
   databaseURL: 'http://localhost:8080'
@@ -14,7 +16,7 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 });
 
 export const addMessage = functions.https.onRequest(async (request, response) => {
-  if(request.method != 'POST') response.send('Need a POST message to send request');
+  if (request.method != 'POST') response.send('Need a POST message to send request');
   let original = request.query.text;
   console.log(request.query);
   const writeResult = await admin.firestore().collection("messages").add({ original: original });
@@ -27,12 +29,18 @@ exports.saveMaintenanceDataToFirestore = functions.https.onRequest(async (req, r
     console.log(req.body);
     // Get the data from the request body
     const data = req.body;
+    // Convert "WorkDate" field to a Firestore Timestamp
+    if (data["WorkDate"]) {
+      const dateConvert = new Date(data["WorkDate"]);
+      const timeStamp = Timestamp.fromDate(dateConvert); // Use Timestamp from the correct package
+      data.WorkDate = timeStamp;
+    }
 
     // Save the data to Firestore
     const docRef = await admin.firestore().collection(COLLECTION_NAME).add(data);
 
     // Return the ID of the newly created document
-    res.json({  result: `Data with ID: ${docRef.id} was added` });
+    res.json({ result: `Data with ID: ${docRef.id} was added` });
   } catch (error) {
     // Handle any errors that occur
     console.error('Error saving data to Firestore:', error);
@@ -42,12 +50,16 @@ exports.saveMaintenanceDataToFirestore = functions.https.onRequest(async (req, r
 
 export const getSpecificStoreData = functions.https.onRequest(async (req, res) => {
   try {
+    //Field Name we are filtering on
     const fieldName = 'StoreName';
+    //Grabs the field value from request query 
     const fieldValue = req.query.fieldValue;
 
+    //Queries the Firestore collection based on the field value
     const querySnapshot = await admin.firestore().collection(COLLECTION_NAME).where(fieldName, '==', fieldValue).get();
+    //Grabs all the docs that were filtered and stores it into an object to pass back
     const data = querySnapshot.docs.map((doc) => doc.data());
-
+    //Send the json data back
     res.status(200).json(data);
   } catch (error) {
     console.error('Error retrieving data:', error);
@@ -55,6 +67,59 @@ export const getSpecificStoreData = functions.https.onRequest(async (req, res) =
   }
 });
 
+export const updateDataByField = functions.https.onRequest(async (req, res) => {
+  try {
+    // Extract the field value from the request body
+    const fieldValue = req.body.fieldValue;
+
+    // Extract the updated data from the request body
+    const updatedData = req.body.updatedData;
+
+    // Query the Firestore collection based on the field value
+    const querySnapshot = await admin.firestore().collection(COLLECTION_NAME).where('StoreName', '==', fieldValue).get();
+
+    // Update the documents with the updated data
+    const batch = admin.firestore().batch();
+    querySnapshot.forEach((doc) => {
+      const docRef = doc.ref;
+      batch.update(docRef, updatedData);
+    });
+
+    // Commit the batch write
+    await batch.commit();
+
+    res.status(200).send('Data updated successfully');
+  } catch (error) {
+    console.error('Error updating data:', error);
+    res.status(500).send('Error updating data');
+  }
+});
+
+export const getObjectsByDateRange = functions.https.onRequest(async (request, response) => {
+  const startDate = request.query.startDate as string;
+  const endDate = request.query.endDate as string;
+  const storeName = request.query.storeName as string;
+  try {
+    const snapshot = await admin.firestore().collection(COLLECTION_NAME)
+      .where('StoreName', '==', storeName)
+      .where('WorkDate', '>=', new Date(startDate))
+      .where('WorkDate', '<=', new Date(endDate))
+      .get();
+
+    const objects: any[] = [];
+    snapshot.forEach((doc) => { 
+      const data = doc.data();
+      const workDateTimestamp = data.WorkDate.toDate();
+      const updatedData = { ...data, WorkDate: workDateTimestamp };
+      objects.push(updatedData);
+    });
+
+    response.json(objects);
+  } catch (error) {
+    console.error('Error getting objects:', error);
+    response.status(500).send('Error getting objects');
+  }
+});
 
 
 
